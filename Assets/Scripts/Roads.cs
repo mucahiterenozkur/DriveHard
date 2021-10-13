d29c43d06971b6902c57274a178e777d7a6ac6ef
Mücahit Eren Özkur <mucahiterenozkur@hotmail.com>
2021-10-14 02:02:51 +0300

    obstacles appears randomly from spawn points now and roads are spawning randomly

---
+++ Assets/Scripts/Roads.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roads : MonoBehaviour
+{
+    public GameObject roadPrefab;
+    Vector3 nextSpawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < 15; i++)
+        {
+            SpawnRoadd();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnRoadd()
+    {
+
+        GameObject temp = Instantiate(roadPrefab, nextSpawnPoint, Quaternion.identity) as GameObject;
+        nextSpawnPoint = temp.transform.GetChild(6).transform.position;
+
+
+    }
+}
--- Assets/Scripts/SpawnRoad.cs
+++ Assets/Scripts/SpawnRoad.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class SpawnRoad : MonoBehaviour
 {
-
+    //public Transform[] spawnPoints;
+    private Roads roads;
+    public GameObject[] obstacles;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        roads = FindObjectOfType<Roads>();
+        StartCoroutine(SpawnObstacless());
     }
 
     // Update is called once per frame
@@ -19,7 +24,46 @@ public class SpawnRoad : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        transform.position += new Vector3(0, 0, transform.GetChild(0).GetComponent<Renderer>().bounds.size.z * 10);
+        roads.SpawnRoadd();
+        Destroy(gameObject, 1f);
+
+        //Destroy(gameObject);
+
+        //transform.position += new Vector3(0, 0, transform.GetChild(0).GetComponent<Renderer>().bounds.size.z * 5);
+        //GameObject newRoad = Instantiate(roadPrefab, currentRoads[0].transform.GetChild(0).GetComponent<Renderer>().bounds.size * 5, Quaternion.identity) as GameObject;
+        //currentRoads.Add(newRoad);
+        //Destroy(gameObject);
+
         //Debug.Log("hit");
     }
+
+    IEnumerator SpawnObstacless()
+    {
+        List<int> whereToPlaced = new List<int>();
+        int numberOfSpawn = Random.Range(3, 5);
+        Debug.Log(numberOfSpawn);
+
+        for (int i = 0; i < numberOfSpawn; i++)
+        {
+            int spawnPlace = Random.Range(1, 6);
+            Transform spawnPoint = transform.GetChild(spawnPlace).transform;
+
+            int whichObstacleToSpawn = Random.Range(0, obstacles.Length);
+
+           
+            if (!whereToPlaced.Contains(spawnPlace))
+            {
+                whereToPlaced.Add(spawnPlace);
+                Instantiate(obstacles[whichObstacleToSpawn], spawnPoint.position, Quaternion.identity, transform);
+            }
+
+                
+        }
+           
+
+        yield return new WaitForSeconds(1f);
+
+    }
+
+
 }
